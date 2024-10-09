ec9a93aa338480edf66e5df7483aaaf6581dc07b
Courtney Armour <courtneyrarmour@gmail.com>
2024-10-09 11:39:31 -0700

    fix mistake

--- R/subset_voom.R
+++ R/subset_voom.R
@@ -16,6 +16,7 @@ data$design
 
 subset_voom <- function(dat_voom, lib_filter = NULL, gene_filter=NULL){ # add library ID column in targets?
 
+  #create subset object
   dat_voom_sub<-dat_voom
 
   # Add gene & sample ID's to weights matrix
@@ -50,11 +51,11 @@ subset_voom <- function(dat_voom, lib_filter = NULL, gene_filter=NULL){ # add li
   message(paste0("Subsetting to ",length(libs_sub)," of ",ncol(dat_voom$E)," libraries"))
   message(paste0("Subsetting to ",length(genes_sub)," of ",nrow(dat_voom$E)," genes"))
 
-  dat_voom_sub$targets <- dat_voom$targets[libs_sub,]
-  dat_voom_sub$genes <- dat_voom$genes[genes_sub,]
-  dat_voom_sub$E <- dat_voom$E[genes_sub,libs_sub]
-  dat_voom_sub$weights <- dat_voom$weights[genes_sub,libs_sub]
-  dat_voom_sub$design <- dat_voom$design[libs_sub,]
+  dat_voom_sub$targets <- dat_voom_sub$targets[libs_sub,]
+  dat_voom_sub$genes <- dat_voom_sub$genes[genes_sub,]
+  dat_voom_sub$E <- dat_voom_sub$E[genes_sub,libs_sub]
+  dat_voom_sub$weights <- dat_voom_sub$weights[genes_sub,libs_sub]
+  dat_voom_sub$design <- dat_voom_sub$design[libs_sub,]
 
   message("Done!")
 
