6d6fb8a97c252a73d11de614ab51807db31567f7
rmsegnitz <43895313+rmsegnitz@users.noreply.github.com>
2024-11-21 13:24:53 -0800

    Update calculate_module_coherence.R
    Minor updates to annotation.

--- R/calculate_module_coherence.R
+++ R/calculate_module_coherence.R
@@ -44,6 +44,7 @@ calculate_module_coherence<-
     # set undefined global variables to null
     X1 <- X2 <- allCorVals<- allPVals <- V3<- Cor<- P <- Set <- gene1<- gene2<- P.est<- h_label<- h_pos<- negLogP<- value<- h_line<- NULL
 
+    # convenience define module definition dataframe and check data formatting
     moduleSets<- module_gene_sets
     if(class(voom_obj)[1]== "EList"){voomData<-voom_obj$E}else{voomData<-voom_obj}
     if(!is.numeric(voomData)){
@@ -52,16 +53,19 @@ calculate_module_coherence<-
         Be advised to check for stray metadata in the expression matrix.")}
 
 
-    #---------------------------------------------------------
+    #-----------------------------------------------------------------
     #  Calculate the gene set means (module expression)
-    #---------------------------------------------------------
+    #  [this is currently not used, but may be incorporated later]
+    #-----------------------------------------------------------------
     uniGS<-moduleSets%>%
       dplyr::pull(gene_set_column)%>%
       unique()%>%as.character()%>%gtools::mixedsort() # Pull unique modules & order correctly
 
+    # establish storage
     GSsub<-c()
     GSabsent<-list()
 
+    # for each geneset pull corresponding gene indices and compute means
     for (i in uniGS){
       curGS<-i
       curIDs<-as.character(moduleSets[which(moduleSets[, gene_set_column]==curGS), ensembl_ID_column])
@@ -76,14 +80,13 @@ calculate_module_coherence<-
         GSsub<-rbind(GSsub, voomData[matchIndex,])}
     }
 
-    #rownames(GSsub)<-uniGS
-
+    # print message if genes are missing from genesets.
     if(length(unlist(GSabsent))>0){
       print(paste0(length(unique(unlist(GSabsent))), " Module Genes absent in voom object. Missing genes accesible in .$GSabsent"))}
 
-    #----------------------------
+    #-------------------------------------------
     #  Set function for correlation p values
-    #----------------------------
+    #-------------------------------------------
 
     # Correlation p value function
     cor2pvalue = function(r, n) {
@@ -104,6 +107,7 @@ calculate_module_coherence<-
     SubGeneCorMedian<-c()
     SubGenePMedian<-c()
 
+    # for each geneset pull the correct gene IDs and indices, calculate correlation matrices, and format results.
     for (i in uniGS){
       curGS<-i
       curIDs<-as.character(moduleSets[which(moduleSets[, gene_set_column]==curGS), ensembl_ID_column])
@@ -111,11 +115,11 @@ calculate_module_coherence<-
       if (any(is.na(matchIndex))){
         matchIndex<-matchIndex[-which(is.na(matchIndex))]}
 
-      if(length(matchIndex)>1){
+      if(length(matchIndex)>1){ # run correlation calculations as long as modules contain multiple genes
         setCor<-stats::cor(t(voomData[matchIndex,])) # Calculate pairwise gene correlations from expression data
         n<-t(!is.na(t(voomData[matchIndex,]))) %*% (!is.na(t(voomData[matchIndex,]))) # create matrix with sample size to calculate correlation p (dim=curIDs*curIDs, value = sample size)
 
-        allCorInfo<-cor2pvalue(setCor, n)
+        allCorInfo<-cor2pvalue(setCor, n) # calculate correlation p values from matrix.
         setP<-allCorInfo$p
 
         SubGeneCorDF[[i]]<- # Assemble df of gene pairs, correlations, and p values
@@ -163,10 +167,12 @@ calculate_module_coherence<-
                      plot.subtitle = ggplot2::element_text(hjust=0.5),
                      axis.text.x = ggplot2::element_text(angle=45, hjust=1, vjust=1))
 
+    # define plot labels
     labels_df_P<-data.frame(name=rep("negLogP", length(p_cutoff)),
                             h_pos = -log10(p_cutoff)-0.2,
                             h_label = paste("p=",p_cutoff,sep=""))
 
+    #Boxplot of all pairwise correlations pvals per module
     coherence_boxplot_p<-
       SubGeneCorDF%>%
       dplyr::mutate(P.est = ifelse(P==0, sort(unique(SubGeneCorDF$P))[2], P)) %>% #Fill in true 0 with lowest P-value in dataset
@@ -186,12 +192,14 @@ calculate_module_coherence<-
                      plot.subtitle = ggplot2::element_text(hjust=0.5),
                      axis.text.x = ggplot2::element_text(angle=45, hjust=1, vjust=1))
 
-    # Or with facets
+    # Plot equivalent with facets
+    # define plot labels
     labels_df<-data.frame(name=c(rep("Cor", length(r_cutoff)), rep("negLogP", length(p_cutoff))),
                           h_line = c(r_cutoff, -log10(p_cutoff)),
                           h_pos = c(r_cutoff-0.02, -log10(p_cutoff)-0.2),
                           h_label = c(paste("r=",r_cutoff,sep=""), paste("p=",p_cutoff,sep="")))
 
+    #Boxplot of all pairwise correlations & pvals per module
     coherence_boxplot_faceted<-
       SubGeneCorDF%>%
       dplyr::mutate(P.est = ifelse(P==0, sort(unique(SubGeneCorDF$P))[2], P)) %>% #Fill in true 0 with lowest P-value in dataset
@@ -218,7 +226,7 @@ calculate_module_coherence<-
       ggplot2::facet_wrap(~name, scales = "free_y", strip.position = "left",
                           labeller = ggplot2::labeller(name = c("Cor" = "Correlation Strength", "negLogP" = "-log10(Correlation P Value)")))
 
-
+# Format results for return
     if(return_plot){print(coherence_boxplot_faceted)}
 
     if(length(unlist(GSabsent))>0){
